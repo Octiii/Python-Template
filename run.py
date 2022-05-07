126f4aae128b610b90351d4add017fa95d912885
Octiii <sirocti@gmail.com>
2022-05-07 14:04:35 +0000

    Bunch of stuff.

--- run.py
+++ run.py
@@ -16,14 +16,19 @@ def get_sales_data():
     """
     Get sales figure input from user 
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by a comma.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by a comma.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+    return sales_data
 
 def validate_data(values):
     """
@@ -32,14 +37,15 @@ def validate_data(values):
     or if there aren't exactley 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactley 6 values requiered, you provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, plese try again.\n")
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True       
 
 
-    print(values)
-
-get_sales_data()
+data = get_sales_data()
