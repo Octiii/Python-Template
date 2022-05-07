c83a977e89968c82ee775db84fe44b84ef2e87dd
Octiii <sirocti@gmail.com>
2022-05-07 12:57:01 +0000

    Added validation for user data to be 6 values long

--- run.py
+++ run.py
@@ -21,6 +21,25 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Converts string values into integers.
+    Raises VAlueError if string cannot be converted into int,
+    or if there aren't exactley 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactley 6 values requiered, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, plese try again.\n")
+
+
+    print(values)
 
 get_sales_data()
